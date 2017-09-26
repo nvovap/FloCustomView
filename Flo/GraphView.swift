1ad3948441cb11e46a23248c968ac244427f96e2
Vladimir Nevinniy <nvovap@gmail.com>
2017-09-26 17:40:40 +0300

    first commit

---
+++ Flo/GraphView.swift
@@ -0,0 +1,155 @@
+//
+//  GraphView.swift
+//  Flo
+//
+//  Created by Vladimir Nevinniy on 9/26/17.
+//  Copyright © 2017 Vladimir Nevinniy. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class GraphView: UIView {
+    
+    //Weekly sample data
+    var graphPoints = [4, 2, 6, 4, 5, 8, 3]
+    
+    private struct Constants {
+        static let cornerRadiusSize = CGSize(width: 8.0, height: 8.0)
+        static let margin: CGFloat = 20.0
+        static let topBorder: CGFloat = 60
+        static let bottomBorder: CGFloat = 50
+        static let colorAlpha: CGFloat = 0.3
+        static let circleDiameter: CGFloat = 5.0
+    }
+
+    @IBInspectable var startColor: UIColor = .red
+    @IBInspectable var endColor: UIColor = .green
+    
+    override func draw(_ rect: CGRect) {
+        
+        let width = rect.width
+        let height = rect.height
+        
+        let path = UIBezierPath(roundedRect: rect,
+                                byRoundingCorners: .allCorners,
+                                cornerRadii: Constants.cornerRadiusSize)
+        path.addClip()
+        
+        // 2
+        let context = UIGraphicsGetCurrentContext()!
+        let colors = [startColor.cgColor, endColor.cgColor]
+        
+        // 3
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        
+        // 4
+        let colorLocations: [CGFloat] = [0.0, 1.0]
+        
+        // 5
+        let gradient = CGGradient(colorsSpace: colorSpace,
+                                  colors: colors as CFArray,
+                                  locations: colorLocations)!
+        
+        // 6
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: bounds.height)
+        context.drawLinearGradient(gradient,
+                                   start: startPoint,
+                                   end: endPoint,
+                                   options: [])
+        
+        
+        let margin = Constants.margin
+        let graphWidth = width - margin * 2 - 4
+        let columnXPoint = { (column: Int) -> CGFloat in
+            //Calculate the gap between points
+            let spacing = graphWidth / CGFloat(self.graphPoints.count - 1)
+            return CGFloat(column) * spacing + margin + 2
+        }
+        
+        let topBorder = Constants.topBorder
+        let bottomBorder = Constants.bottomBorder
+        let graphHeight = height - topBorder - bottomBorder
+        let maxValue = graphPoints.max()!
+        let columnYPoint = { (graphPoint: Int) -> CGFloat in
+            let y = CGFloat(graphPoint) / CGFloat(maxValue) * graphHeight
+            return graphHeight + topBorder - y // Flip the graph
+        }
+        
+        UIColor.white.setFill()
+        UIColor.white.setStroke()
+        
+        // set up the points line
+        let graphPath = UIBezierPath()
+        
+        // go to start of line
+        graphPath.move(to: CGPoint(x: columnXPoint(0), y: columnYPoint(graphPoints[0])))
+        
+        // add points for each item in the graphPoints array
+        // at the correct (x, y) for the point
+        for i in 1..<graphPoints.count {
+            let nextPoint = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            graphPath.addLine(to: nextPoint)
+        }
+        
+        graphPath.stroke()
+        
+        //1 - save the state of the context (commented out for now)
+        context.saveGState()
+        
+        
+        let clippingPath = graphPath.copy() as! UIBezierPath
+        
+        //3 - add lines to the copied path to complete the clip area
+        clippingPath.addLine(to: CGPoint(x: columnXPoint(graphPoints.count - 1), y:height))
+        clippingPath.addLine(to: CGPoint(x:columnXPoint(0), y:height))
+        clippingPath.close()
+        
+        //4 - add the clipping path to the context
+        clippingPath.addClip()
+        
+        let highestYPoint = columnYPoint(maxValue)
+        let graphStartPoint = CGPoint(x: margin, y: highestYPoint)
+        let graphEndPoint = CGPoint(x: margin, y: bounds.height)
+        
+        context.drawLinearGradient(gradient, start: graphStartPoint, end: graphEndPoint, options: [])
+        
+        context.restoreGState()
+        //Draw the circles on top of the graph stroke
+        for i in 0..<graphPoints.count {
+            var point = CGPoint(x: columnXPoint(i), y: columnYPoint(graphPoints[i]))
+            point.x -= Constants.circleDiameter / 2
+            point.y -= Constants.circleDiameter / 2
+            
+            let circle = UIBezierPath(ovalIn: CGRect(origin: point, size: CGSize(width: Constants.circleDiameter, height: Constants.circleDiameter)))
+            circle.fill()
+        }
+        
+        
+        //Draw horizontal graph lines on the top of everything
+        let linePath = UIBezierPath()
+        
+        //top line
+        linePath.move(to: CGPoint(x: margin, y: topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: topBorder))
+        
+        //center line
+        linePath.move(to: CGPoint(x: margin, y: graphHeight/2 + topBorder))
+        linePath.addLine(to: CGPoint(x: width - margin, y: graphHeight/2 + topBorder))
+        
+        //bottom line
+        linePath.move(to: CGPoint(x: margin, y:height - bottomBorder))
+        linePath.addLine(to: CGPoint(x:  width - margin, y: height - bottomBorder))
+        let color = UIColor(white: 1.0, alpha: Constants.colorAlpha)
+        color.setStroke()
+        
+        linePath.lineWidth = 1.0
+        linePath.stroke()
+        
+        
+        
+    }
+
+}
--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    
+    //Label outlets
+    @IBOutlet weak var averageWaterDrunk: UILabel!
+    @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    
+    var isGraphViewShowing = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        counterLabel.text = String(counterView.counter)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func counterViewTap(_ sender: UITapGestureRecognizer?) {
+        
+        if isGraphViewShowing {
+            UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        } else {
+            UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
+        }
+        
+        isGraphViewShowing = !isGraphViewShowing
+        
     }
+    
 
-
+    @IBAction func pushButtonPressed(_ button: PushButton) {
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+        
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        
+        counterLabel.text = String(counterView.counter)
+    }
+    
 }
 
