82e9f0231bf4d4e2598b7673147d9839e4c830c3
Vladimir Nevinniy <nvovap@gmail.com>
2017-09-27 15:34:58 +0300

    27092017

--- Flo/CounterView.swift
+++ Flo/CounterView.swift
@@ -35,6 +35,9 @@ class CounterView: UIView {
     
     override func draw(_ rect: CGRect) {
         
+        
+        
+        
         // 1
         let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
         
@@ -88,6 +91,41 @@ class CounterView: UIView {
         outlineColor.setStroke()
         outlinePath.lineWidth = Constants.lineWidth
         outlinePath.stroke()
+        
+        
+        //Counter View markers
+        let context = UIGraphicsGetCurrentContext()!
+        
+        //1 - save original state
+        context.saveGState()
+        outlineColor.setFill()
+        
+        let markerWidth: CGFloat = 5.0
+        let markerSize: CGFloat = 10.0
+        
+        //2 - the marker rectangle positioned at the top left
+        let markerPath = UIBezierPath(rect: CGRect(x: -markerWidth / 2, y: 0, width: markerWidth, height: markerSize))
+        
+        //3 - move top left of context to the previous center position
+        context.translateBy(x: rect.width / 2, y: rect.height / 2)
+        
+        for i in 1...Constants.numberOfGlasses {
+            //4 - save the centred context
+            context.saveGState()
+            //5 - calculate the rotation angle
+            let angle = arcLengthPerGlass * CGFloat(i) + startAngle - .pi / 2
+            //rotate and translate
+            context.rotate(by: angle)
+            context.translateBy(x: 0, y: rect.height / 2 - markerSize)
+            
+            //6 - fill the marker rectangle
+            markerPath.fill()
+            //7 - restore the centred context for the next rotate
+            context.restoreGState()
+        }
+        
+        //8 - restore the original state in case of more painting
+        context.restoreGState()
         
     }
 
