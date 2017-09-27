d87e5130676e153d8d099a7ef60abaa30b829629
Vladimir Nevinniy <nvovap@gmail.com>
2017-09-27 15:12:20 +0300

    111

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -22,7 +22,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var maxLabel: UILabel!
     @IBOutlet weak var stackView: UIStackView!
     
-    var isGraphViewShowing = true
+    var isGraphViewShowing = false
+    
+    
+    func setupGraphDisplay() {
+        
+        let maxDayIndex = stackView.arrangedSubviews.count - 1
+        
+        //  1 - replace last day with today's actual data
+        graphView.graphPoints[graphView.graphPoints.count - 1] = counterView.counter
+        //2 - indicate that the graph needs to be redrawn
+        graphView.setNeedsDisplay()
+        maxLabel.text = "\(graphView.graphPoints.max()!)"
+        
+        //  3 - calculate average from graphPoints
+        let average = graphView.graphPoints.reduce(0, +) / graphView.graphPoints.count
+        averageWaterDrunk.text = "\(average)"
+        
+        // 4 - setup date formatter and calendar
+        let today = Date()
+        let calendar = Calendar.current
+        
+        let formatter = DateFormatter()
+        formatter.setLocalizedDateFormatFromTemplate("EEEEE")
+        
+        // 5 - set up the day name labels with correct days
+        for i in 0...maxDayIndex {
+            if let date = calendar.date(byAdding: .day, value: -i, to: today),
+                let label = stackView.arrangedSubviews[maxDayIndex - i] as? UILabel {
+                label.text = formatter.string(from: date)
+            }
+        }
+    }
     
     
     override func viewDidLoad() {
@@ -36,6 +67,7 @@ class ViewController: UIViewController {
         if isGraphViewShowing {
             UIView.transition(from: graphView, to: counterView, duration: 1.0, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
         } else {
+            setupGraphDisplay()
             UIView.transition(from: counterView, to: graphView, duration: 1.0, options: [.transitionFlipFromRight, .showHideTransitionViews], completion: nil)
         }
         
